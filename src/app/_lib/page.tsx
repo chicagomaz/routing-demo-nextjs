41121fb75c56e9201efc305d9293796135ced02a
Maximlian <chicagomaz13@gmail.com>
2024-11-05 13:02:54 -0500

    first commit

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRouter(){
+    return <h1>You cannot view this in browser</h1>
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: "About Mungoid"
+}
+
+export default function About(){
+    return <h1>About me</h1>
+}
